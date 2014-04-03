bdb3d8e340400469ef7a1b1986d3b085c65425ef
Sean Taylor <sean@freerunningtechnologies.com>
2014-04-03 16:52:32 -0700

    add notifications task for expiring cards
    Change-Id: Ia173d65427f6be78ea76a8ed0d31f45c4439d738

--- lib/tasks/spree_gift_card.rake
+++ lib/tasks/spree_gift_card.rake
@@ -1,1 +1,8 @@
-# add custom rake tasks here
+require "gift_card_notification"
+
+namespace :spree_gift_card do
+  desc "Send a expiring soon email to emails of all gift cards soon to expire"
+  task send_expiration_notifications: :environment do
+    GiftCardNotification.send_notifications!(7)
+  end
+end
---
+++ spec/lib/gift_card_notification_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe GiftCardNotification do
+  describe ".send_notifications!" do
+    let (:gift_card) { create(:gift_card, expiration_date: 7.days.from_now) }
+
+    it "calls mailer for expiring gift_card" do
+      mailer = double
+      mailer.should_receive(:deliver)
+      expect(Spree::GiftCardMailer).to receive(:gift_card_expiring).with(an_instance_of(Spree::GiftCard)).and_return(mailer)
+
+      GiftCardNotification.send_notifications!(7)
+    end
+  end
+end
