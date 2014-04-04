add6411641bdf736443072ed9c380082d67584bc
Dylan Kendal <dkendal@freerunningtechnologies.com>
2014-04-03 17:35:19 -0700

    spree::users_controller#gift_cards action
    show all giftcards for the current user, add ?show_all to query string
    to include expired giftcards as well.
    
    Change-Id: I216afd6c32a07957363f7263f63f0006a259ef7d

--- spec/factories/gift_cards.rb
+++ spec/factories/gift_cards.rb
@@ -5,5 +5,11 @@ FactoryGirl.define do
     variant
     line_item
     expiration_date '2030-04-01'
+
+    trait :expired do
+      expiration_date Time.now - 1.day
+    end
+
+    factory :expired_gc, traits: [:expired]
   end
 end
