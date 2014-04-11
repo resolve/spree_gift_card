475274791fddcbcb13f7cffce7974c393751353a
Dylan Kendal <dkendal@freerunningtechnologies.com>
2014-04-10 17:11:04 -0700

    moved giftcard route out of user controller decorator
    moved it into the giftcard controller as the index action
    
    Change-Id: I9710ba3e4b39e4c47b5e4ffc10a08dbda8dfc52c

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Spree::Core::Engine.routes.draw do
   resources :gift_cards
 
-  get "/account/gift_cards", to: "users#gift_cards", as: :account_gift_cards
+  resource :account, controller: 'users' do
+    resources :gift_cards
+  end
 
   namespace :admin do
     resources :gift_cards do
