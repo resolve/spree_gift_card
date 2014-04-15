f3965754024f7d7da49ec355794cd8ac70968022
Dylan Kendal <dkendal@freerunningtechnologies.com>
2014-04-15 13:58:55 -0700

    changes gc factory to not create line item and variant
    user gift_card_product factory instead if you want to simulate it as
    part of an order
    
    Change-Id: I5944fc7e5481cec0b69b392e8a19ebdf85b6571c

--- spec/factories/gift_cards.rb
+++ spec/factories/gift_cards.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :gift_card, class: Spree::GiftCard do
     email 'spree@example.com'
     name 'Example User'
-    variant
-    line_item
+    current_value 19.99
+    original_value 19.99
     expiration_date Time.now + 2.months
 
     trait :redeemed do
@@ -17,6 +17,12 @@ FactoryGirl.define do
       expiration_date Time.now - 1.day
     end
 
+    trait :for_order do
+      variant
+      line_item
+    end
+
+    factory :gift_card_product, traits: [:for_order]
     factory :redeemed_gc, traits: [:redeemed]
     factory :expired_gc, traits: [:expired]
   end
