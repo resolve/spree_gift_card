8d5fe9344254a6aac7f3fd2a56e45452613e2581
Richard Wilson <richard@freerunningtech.com>
2014-04-23 17:05:54 -0700

    Fix calculator impl

--- spec/models/spree/calculator/gift_card_spec.rb
+++ spec/models/spree/calculator/gift_card_spec.rb
@@ -1,29 +1,38 @@
 require 'spec_helper'
 
 describe Spree::Calculator::GiftCard do
-
-  let(:calculator) { Spree::Calculator::GiftCard.new }
-  let(:gift_card) { create(:gift_card, variant: create(:variant, price: 25)) }
-  let(:order) { mock_model Spree::Order, adjustments: [], item_total: 10, ship_total: 5, tax_total: 1 }
-
   it '.description' do
     Spree::Calculator::GiftCard.description.should eql('Gift Card Calculator')
   end
 
   describe '#compute' do
-    it "should compute 0 if current value is 0" do
-      gift_card.stub :current_value => 0
-      calculator.compute(order, gift_card).should == 0
+    let(:order) { create :order_ready_to_ship}
+    let(:applied_gc) do
+      Spree::GiftCard.create!(
+        name: "test",
+        email: "t@t.t",
+        current_value: 10,
+        original_value: 10
+      )
     end
 
-    it "should compute amount correctly when order total less than current value" do
-      calculator.compute(order, gift_card).should == -16
+    let(:gc) do
+      Spree::GiftCard.create!(
+        name: "test",
+        email: "t@t.t",
+        current_value: 50,
+        original_value: 50
+      )
     end
 
-    it "should compute amount correctly when order total greater than current value" do
-      order.stub :item_total => 25
-      calculator.compute(order, gift_card).should == -25
+    before do
+      applied_gc.apply(order)
+    end
+
+    subject { gc.calculator }
+
+    it "only redeems $40 from the second gc as the order total is 50." do
+      expect(subject.compute(order, gc).to_f).to eql(-40.0)
     end
   end
-
 end
