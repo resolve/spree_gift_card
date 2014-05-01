00f2b825b3fca6fe8532dbc6a2c4718f2f492568
Richard Wilson <richard@freerunningtech.com>
2014-05-01 10:57:36 -0700

    Show gift cards on admin/orders#edit

--- app/models/spree/adjustment_decorator.rb
+++ app/models/spree/adjustment_decorator.rb
@@ -1,5 +1,4 @@
 Spree::Adjustment.class_eval do
-
   scope :gift_card, -> { where(:originator_type => 'Spree::GiftCard') }
-
+  scope :non_gift_card, ->{ where.not(originator_type: "Spree::GiftCard") }
 end
