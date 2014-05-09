44d14427d96e1dcd97db15d17e2a1e2f24bf8840
Richard Wilson <richard@freerunningtech.com>
2014-05-09 16:04:25 -0700

    Grab the tax from the line items manually.

--- app/models/spree/calculator/gift_card.rb
+++ app/models/spree/calculator/gift_card.rb
@@ -12,7 +12,7 @@ module Spree
         order_adjustments = order.adjustments.where("amount < 0")
         order_adjustments.delete_if {|adj| adj.source == gift_card }
         credits = order_adjustments.map(&:amount).sum
-        order_total = order.item_total + order.ship_total + order.additional_tax_total + credits
+        order_total = order.item_total + order.ship_total + order.line_items.map(&:additional_tax_total).sum + credits
         [order_total, gift_card.current_value].min * -1
       end
     end
