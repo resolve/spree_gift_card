551f78dc4d6b788661812fa960a473adc204ad80
Richard Wilson <richard@freerunningtech.com>
2014-05-09 16:26:03 -0700

    Fix purchase spec
    This includes a fairly weak override, but there's not much of a better
    way of targeting it. At this point all tests are passing.

--- app/overrides/spree/admin/orders/modify_adjustment_display.rb
+++ app/overrides/spree/admin/orders/modify_adjustment_display.rb
@@ -1,7 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/orders/_form',
-  name: "partialize_adjustment_table",
-  replace_contents: "erb[silent]:contains('if order.adjustments.eligible.exists?')",
-  closing_selector: "erb[silent]:contains('end')",
+  name: "exclude_gc_adjustments_from_order_adjustments",
+  replace: "erb[loud]:contains(':adjustments => @order.adjustments')",
   partial: "spree/admin/orders/adjustment_table"
 )
