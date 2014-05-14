7b15c1344ec2983951e10a0119e08c9ac1c8b585
Richard Wilson <richard@freerunningtech.com>
2014-05-14 10:31:56 -0700

    Fix specs due to factory data changes.

--- spec/models/spree/calculator/gift_card_spec.rb
+++ spec/models/spree/calculator/gift_card_spec.rb
@@ -7,23 +7,9 @@ describe Spree::Calculator::GiftCard do
 
   describe '#compute' do
     let(:order) { create :order_ready_to_ship}
-    let(:applied_gc) do
-      Spree::GiftCard.create!(
-        name: "test",
-        email: "t@t.t",
-        current_value: 10,
-        original_value: 10
-      )
-    end
+    let(:applied_gc) { create :gift_card, current_value: 110 }
 
-    let(:gc) do
-      Spree::GiftCard.create!(
-        name: "test",
-        email: "t@t.t",
-        current_value: 50,
-        original_value: 50
-      )
-    end
+    let(:gc) { create :gift_card, current_value: 80 }
 
     before do
       applied_gc.apply(order)
@@ -31,7 +17,7 @@ describe Spree::Calculator::GiftCard do
 
     subject { gc.calculator }
 
-    it "only redeems $40 from the second gc as the order total is 50." do
+    it "only redeems $40 from the second gc as the order total is 150." do
       expect(subject.compute(order, gc).to_f).to eql(-40.0)
     end
   end
