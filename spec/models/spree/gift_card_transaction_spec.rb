75f8c1cf2d1a728d9463cd91ef9c41254439f178
Richard Wilson <richard@freerunningtech.com>
2014-05-14 14:28:36 -0700

    Axe transactions.
    We dont use them

--- spec/models/spree/gift_card_transaction_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Spree::GiftCardTransaction do
-  it {should belong_to(:gift_card)}
-  it {should belong_to(:order)}
-  it {should validate_presence_of(:amount)}
-  it {should validate_presence_of(:gift_card)}
-  it {should validate_presence_of(:order)}
-end
