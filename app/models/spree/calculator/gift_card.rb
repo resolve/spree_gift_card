7d23a4d2fd2b6ddcb816ff9af0abdf7043733208
Richard Wilson <richard@freerunningtech.com>
2014-05-16 13:52:06 -0700

    Refactor calculator to ensure an up to date adjustment total.
    Unfortunately, we need an up to date adjustment total to apply gift
    cards correctly. This is not something the calculator knows about, as
    adjustments can be added that would change the total before the totals
    are persisted on the order. So we have to recalculate that total
    ourselves.

--- app/models/spree/calculator/gift_card.rb
+++ app/models/spree/calculator/gift_card.rb
@@ -9,10 +9,16 @@ module Spree
       end
 
       def compute(order, gift_card)
-        order_adjustments = order.adjustments
-        order_adjustments.delete_if {|adj| adj.source == gift_card }
-        credits = order_adjustments.map(&:amount).sum
-        order_total = order.item_total + order.ship_total + order.line_items.map(&:additional_tax_total).sum + credits
+        # order_total is item_total + shipment_total + adjustment_total
+        # at any time this calculator is called, item_total and shipment_total won't change
+        # so we only need to recaclulate adjustment_total for up to date information.
+        order_adjustments = order.adjustments.eligible.to_a
+        order_adjustments.delete_if { |adj| adj.source == gift_card }
+
+        adj_total = order.line_items.sum(:adjustment_total) +
+          order.shipments.sum(:adjustment_total) +
+          order_adjustments.map(&:amount).sum
+        order_total = order.item_total + order.ship_total + adj_total
         [order_total, gift_card.current_value].min * -1
       end
     end
